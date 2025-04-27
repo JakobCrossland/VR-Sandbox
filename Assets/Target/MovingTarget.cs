ffae099c2002d9e33d9dd9c4e1b87af80d81274d
Drakon867 <114099584+Drakon867@users.noreply.github.com>
2025-04-27 12:55:15 -0400

    Updated Targets
    Now with variable score & movement

---
+++ Assets/Target/MovingTarget.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    public Rigidbody MyRig;
+    public int direction;
+    public bool up;
+    public int speed;
+    public int timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRig = GetComponent<Rigidbody>();
+        StartCoroutine(Cycle());
+    }
+
+    public IEnumerator Cycle()
+    {
+        yield return new WaitForSeconds(timer);
+        if (up)
+        {
+            up = false;
+        }
+        else
+        {
+            up = true;
+        }
+        StartCoroutine(Cycle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(direction == 1)
+        {
+            //vertical
+            if (up)
+            {
+                MyRig.velocity = new Vector3(0, speed, 0);
+            }
+            else
+            {
+                MyRig.velocity = new Vector3(0, -speed, 0);
+            }
+        }
+        else if(direction == 2)
+        {
+            //horizontal
+            if (up)
+            {
+                MyRig.velocity = new Vector3(speed, 0, 0);
+            }
+            else
+            {
+                MyRig.velocity = new Vector3(-speed, 0, 0);
+            }
+        }
+    }
+}
